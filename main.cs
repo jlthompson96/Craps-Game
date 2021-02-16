3081f5983d65d9369164eb070ad4d839b67769c9
Joey Thompson <joeythompson1014@gmail.com>
2021-02-16 14:12:37 -0500

    Initial Upload
    Game has some functionality but not full implemented.

---
+++ main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Craps_Game
+{
+    public class Game
+    {
+        static void Main(string[] args)
+        {
+            Program craps = new Program();
+            craps.playGame();
+        }
+    }
+
+}
