66d2f0be1c39f4526096b6cf5780160425d7190a
Joey Thompson <joeythompson1014@gmail.com>
2021-02-17 19:20:22 -0500

    Renamed Program file and added more functionality

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Craps_Game
+{
+    public class Game
+    {
+        static void Main(string[] args)
+        {
+            Craps craps = new Craps();
+            craps.playCraps();
+        }
+    }
+
+}
